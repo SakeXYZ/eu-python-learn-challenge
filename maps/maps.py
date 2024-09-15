84bcb8ea29f5fee61440b7dfd58c110e8b32d32c
SakeXYZ <islamshamshiev2002@gmail.com>
2024-09-15 21:42:36 +0600

    chenge counter of "," simbol

--- maps/maps.py
+++ maps/maps.py
@@ -5,9 +5,11 @@ class MapExercise:
     @staticmethod
     def rating(list_of_movies: list[dict]) -> float:
         rating_list = []
+
         for item in list_of_movies:
-            if item['rating_kinopoisk'] != '' and item['rating_kinopoisk'] != '0' and len(
-                    str(item['country']).split(',')) >= 2:
+            counter = item.get('country', ' ')
+            if (item['rating_kinopoisk'] != ''
+                    and item['rating_kinopoisk'] != '0' and counter.count(',') >= 1):
                 rating_list.append(item['rating_kinopoisk'])
 
         str_to_float = list(map(float, rating_list))
